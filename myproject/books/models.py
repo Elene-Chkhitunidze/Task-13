d1c30e621f77edd25d2063487faa2205ac38fb95
Elene <elenechkhitunidze@gmail.com>
2025-02-17 23:27:18 +0400

    Add Task 34

---
+++ myproject/books/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    published_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
